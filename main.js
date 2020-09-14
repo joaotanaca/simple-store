c32be9cfb2531393f68f60e4e57dabf1475ae5c1
Tanaca <jtanacar@gmail.com>
2020-09-14 20:24:57 -0300

    Setando projeto com electron e react

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
+const path = require("path");
 
-function createWindow () {
-  // Cria uma janela de navegação.
-  const win = new BrowserWindow({
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+    show: true,
+  });
+  const startURL = isDev
+    ? "http://localhost:3000"
+    : `file://${path.join(__dirname, "../build/index.html")}`;
 
-  // e carrega o arquivo index.html do seu aplicativo.
-  win.loadFile('index.html')
+  mainWindow.loadURL(startURL);
+
+  mainWindow.once("ready-to-show", () => mainWindow.show());
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 }
-
-app.whenReady().then(createWindow)
+app.on("ready", createWindow);
